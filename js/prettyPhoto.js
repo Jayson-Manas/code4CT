0b8f0ad1775598bdd6c136fad61beabc75214a06
Jayson <jaysonmanas@gmail.com>
2019-01-24 14:33:13 +0200

    added next and preview button

--- js/prettyPhoto.js
+++ js/prettyPhoto.js
@@ -6,3 +6,41 @@ $(document).ready(function(){
 
     
 });
+
+function changeGrid1(){
+    let x = document.getElementById('main');
+    x.style.display = "inline";
+
+    let y = document.getElementById('main2');
+    y.style.display = "none";
+
+    let z = document.getElementById('main3');
+    z.style.display = "none";
+    
+
+}
+
+function changeGrid2(){
+    let x = document.getElementById('main');
+    x.style.display = "none";
+
+    let y = document.getElementById('main2');
+    y.style.display = "inline";
+
+    let z = document.getElementById('main3');
+    z.style.display = "none";
+    
+
+}
+function changeGrid3(){
+    let x = document.getElementById('main');
+    x.style.display = "none";
+
+    let y = document.getElementById('main2');
+    y.style.display = "none";
+
+    let z = document.getElementById('main3');
+    z.style.display = "inline";
+    
+
+}

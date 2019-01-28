f716397d01fb58e123f5d042f9e6465f93770de2
Jayson-Manas <jaysonmanas@gmail.com>
2019-01-28 22:55:03 +0200

    scroll to top button add to courses page

---
+++ js/counter.js
@@ -0,0 +1,31 @@
+// ====================counter=======================
+var timeLeftForCoffee=0;
+var timeLeftForGirls=0;
+var timeLeftForProjects=0;
+var timeLeftForHours=0;
+
+var cupsOfCoffee=document.getElementById("cupsOfCoffee");
+var girls=document.getElementById("girls");
+var projects=document.getElementById("projects");
+var hoursCoding=document.getElementById("hoursCoding");
+
+var timer=setInterval(function() {
+
+cupsOfCoffee.innerHTML=timeLeftForCoffee;
+girls.innerHTML=timeLeftForGirls;
+projects.innerHTML=timeLeftForProjects;
+hoursCoding.innerHTML=timeLeftForHours;
+
+if (timeLeftForCoffee===3000) {
+clearInterval(timer);
+cupsOfCoffee.innerHTML="3000+";
+girls.innerHTML="780+";
+projects.innerHTML="3120+";
+hoursCoding.innerHTML="8765+";
+}
+timeLeftForGirls++;
+timeLeftForCoffee+=10;
+timeLeftForProjects++;
+timeLeftForHours++;
+},
+1);
